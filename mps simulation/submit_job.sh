2662cb924a6baa4c509b4c5bdd2a5fa845b5ba18
Feng Qian <fengqian5525@gmail.com>
2025-09-05 11:28:20 -0400

    ready for cluster

---
+++ mps simulation/submit_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -p batch
+#SBATCH --job-name fe2s2
+#SBATCH -N 1 # nodes requested
+#SBATCH -n 1 # tasks requested
+#SBATCH -c 5 # cores requested
+#SBATCH -t 3-00:00:00
+#SBATCH --mem=500000 # memory in Mb
+#SBATCH -o outputfile.out # send stdout to outfile
+#SBATCH -e errfile.out  # send stderr to errfile
+module load anaconda/2024.06-py312
+source activate /cluster/tufts/lovelab/fqian03/condaenv/symmer
+python ground_state_solver.py 1>out 2>error
